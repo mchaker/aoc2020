36f281f38a201d782701e13251522a04d8f5964c
M <mchaker@users.noreply.github.com>
2020-12-09 11:30:09 -0800

    day2 start forgot stage this file

--- day2/main.go
+++ day2/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 )
 
 func main() {
@@ -21,34 +20,10 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		lineInt, _ := strconv.Atoi(line)
-		inputs = append(inputs, lineInt)
+		fmt.Println(line)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
-	}
-
-	var inputs2 []int
-	copy(inputs2, inputs)
-
-	for i := range inputs {
-		for j := range inputs {
-			if inputs[i]+inputs[j] == 2020 {
-				fmt.Println("FOUND PAIR!: ", inputs[i], inputs[j])
-				fmt.Println("Product of both: ", inputs[i]*inputs[j])
-			}
-		}
-	}
-
-	for i := range inputs {
-		for j := range inputs {
-			for k := range inputs {
-				if inputs[i]+inputs[j]+inputs[k] == 2020 {
-					fmt.Println("FOUND TRIPLE!: ", inputs[i], inputs[j], inputs[k])
-					fmt.Println("Product of three: ", inputs[i]*inputs[j]*inputs[k])
-				}
-			}
-		}
 	}
 }
